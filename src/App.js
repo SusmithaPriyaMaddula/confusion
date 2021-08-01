c3d051332b8a81bac56cbd81fe3eee3f610fd2e3
SUSMITHA PRIYA <susmithamaddula0@gmail.com>
2021-08-01 22:10:01 +0530

    firstassignment

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
-import React,{Component} from 'react';
-//import Menu from './components/MenuComponent';
+import './App.css';
+import { DISHES } from './shared/dishes'
 
-function App(){
-    constructor(props){
-      super(props);
-      this.state = {
-        dishes: DISHES
-      };
-    }
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-      </div>
-    );
-}
-
-/*
-class App extends Component {
-  constructor(props) {
+class App extends Component{
+  constructor(props){
     super(props);
     this.state = {
       dishes: DISHES
     };
+  } 
+  render(){
+    return(
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+              <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
+          </div>
+        </Navbar>
+
+        <Menu dishes={ this.state.dishes } />
+      </div>
+    );
+
   }
-  return(
-  <Menu dishes={this.state.dishes} />
-  );
-}*/
+
+}
+
 export default App;
